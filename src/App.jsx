b006c90eacef65fa4fb3f01a22dcb6ea3fcb8269
pedrogava <rm551043@fiap.com.br>
2023-09-18 09:31:03 -0300

    rotas criadas

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,26 @@
-import Cabecalho from "./components/Cabecaho";
+import Cabecalho from "./components/Cabecalho";
 import Conteudo from "./components/Conteudo";
 import Rodape from "./components/Rodape";
 import viteLogo from "./assets/vite.svg";
+import style from "./App.module.css";
 
-export default function App(){
+export default function App() {
 
   let viteLogoAlt = "Vite Logo";
-  
-  return(
+
+  return (
     <>
-      <h1>APP - Componente principal!</h1>
-      <div>
+      <h1 className={style.titulo}>APP - Componente principal!</h1>
+      <div className={s}>
+        {/* Iniciando a área do cabeçalho */}
+        <Cabecalho />
 
-        {/*Iniciando a área do cabeçalho */}
-        <Cabecalho/>
-        
-        
-        {/*Iniciando a área do conteudo */}
-        <Conteudo viteLogoProps={viteLogo} viteLogoAltProps={viteLogoAlt}/>   
+        {/* Iniciando a área do conteúdo */}
+        <Conteudo viteLogoProps={viteLogo} viteLogoAltProps={viteLogoAlt}/>
 
-        {/*Iniciando a área do rodapé */}
-        <Rodape/>
+        {/* Iniciando a área do rodapé */}
+        <Rodape />
       </div>
     </>
-  )
+  );
 }
